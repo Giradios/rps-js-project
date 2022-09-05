578cf177264da4c6b4dee14409b851aabff9ba3f
Giradios <jimenez_kai@yahoo.com>
2022-09-05 18:37:59 +0800

    layout

---
+++ java.js
@@ -0,0 +1,18 @@
+/*
+Pesudocode:
+
+get inputFromUser
+get computerChoice
+
+if input and computer = rock + scissors
+    then user wins
+
+if input and computer = paper + scissors
+    then user wins 
+
+etc, etc.
+*/
+
+function getComputerChoice() {
+    
+}
