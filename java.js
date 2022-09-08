706b22747de4457783fdf1937b57562c529e9970
Giradios <jimenez_kai@yahoo.com>
2022-09-08 19:17:53 +0800

    JS: Player and computer inputs

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 /*
-Pesudocode:
+Pseudocode:
 
 get inputFromUser
 get computerChoice
@@ -13,6 +13,33 @@ if input and computer = paper + scissors
 etc, etc.
 */
 
-function getComputerChoice() {
-    
+const windowPrompt = prompt('Rock, Paper, Scissors, Shoot!');
+
+function playerChoice() {
+    const choice = windowPrompt;
+    return choice;
 }
+
+let playerSelection = playerChoice();
+
+/* --------------------------------------- */
+
+function getComputerChoice() {
+    const threeChoices = ['Rock'];
+    const randomSelection = threeChoices[Math.floor(Math.random() * threeChoices.length)];
+
+    return randomSelection;
+}
+
+let computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    const player = playerSelection;
+    const computer = computerSelection; 
+    let comparison = player.toLowerCase() === computer.toLowerCase();
+}
+
+playRound(playerSelection, computerSelection);
+
+console.log('You chose:', playerSelection);
+console.log('The computer chose:', computerSelection);
