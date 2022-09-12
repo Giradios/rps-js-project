a638302b78db943086eef6ac22c3c38c8f2dd09a
Giradios <jimenez_kai@yahoo.com>
2022-09-12 14:59:36 +0800

    logic and loop

--- java.js
+++ java.js
@@ -1,22 +1,11 @@
-/*
-Pseudocode:
 
-get inputFromUser
-get computerChoice
-
-if input and computer = rock + scissors
-    then user wins
-
-if input and computer = paper + scissors
-    then user wins 
-
-etc, etc.
-*/
-
+const array = ['Rock', 'Paper', 'Scissors'];
 const windowPrompt = prompt('Rock, Paper, Scissors, Shoot!');
 
 function playerChoice() {
-    const choice = windowPrompt;
+    const choice = windowPrompt.toLowerCase();
+    console.log('You Chose:', choice)
+
     return choice;
 }
 
@@ -25,21 +14,53 @@ let playerSelection = playerChoice();
 /* --------------------------------------- */
 
 function getComputerChoice() {
-    const threeChoices = ['Rock'];
-    const randomSelection = threeChoices[Math.floor(Math.random() * threeChoices.length)];
+    const randomSelection = array[Math.floor(Math.random() * array.length)].toLowerCase();
+    console.log('Computer Chose:', randomSelection);
 
     return randomSelection;
 }
 
 let computerSelection = getComputerChoice();
 
+/* --------------------------------------- */
+
 function playRound(playerSelection, computerSelection) {
     const player = playerSelection;
     const computer = computerSelection; 
-    let comparison = player.toLowerCase() === computer.toLowerCase();
+    const win = 'You Win This Round!';
+    const lose = 'You Lose This Round!';
+
+    if (player == 'scissors' && computer == 'rock') {
+        console.log(lose);
+    } else if (player == 'scissors' && computer == 'paper') {
+        console.log(win)
+    } else if (player == 'rock' && computer == 'paper') {
+        console.log(lose);
+    } else if (player == 'rock' && computer == 'scissors') {
+        console.log(win);
+    } else if (player == 'paper' && computer == 'scissors') {
+        console.log(lose);
+    } else if (player == 'paper' && computer == 'rock') {
+        console.log(win);
+    } else {
+        location.reload();
+    };
 }
 
 playRound(playerSelection, computerSelection);
 
-console.log('You chose:', playerSelection);
-console.log('The computer chose:', computerSelection);
+/* --------------------------------------- */
+
+function game() {
+    playRound();
+
+    for (let i = 0; i < 5; i++) {
+        if (i = 3) {
+            console.log('You Won The Game!');
+        } else if (i = 4) {
+            console.log('You Lose The Game');
+        }
+    }
+}
+
+game();
